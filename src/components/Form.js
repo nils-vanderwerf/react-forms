80b0d55f467881489993fcf547e40e1bf657bfdd
Nils van der Werf <n.vanderw.92@gmail.com>
2022-02-18 11:05:24 +1100

    Written components, they dont change yet

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,6 @@ class Form extends React.Component {
   state = {
     firstName: "John",
     lastName: "Henry",
-    submittedData: []
   }
 
   handleFirstNameChange = event => {
@@ -19,46 +18,24 @@ class Form extends React.Component {
     })
   }
 
-  handleSubmit = event => {
-    event.preventDefault()
-    let formData = { 
-      firstName: this.state.firstName, 
-      lastName: this.state.lastName 
-    }
-    let dataArray = this.state.submittedData.concat(formData)
-    this.setState({submittedData: dataArray})
-  }
-
-  listOfSubmissions = () => {
-    return this.state.submittedData.map((data, index) => {
-      return (
-        <div key={`${data.firstName}-${data.lastName}-${index}-wrapper`}>
-        <span key={`${data.firstName}-${data.lastName}-${index}`}>{data.firstName} {data.lastName}</span>
-        </div>
-      )
-    })
-  }
-
   render() {
     return (
-      <div>
-        <form onSubmit={event => this.handleSubmit(event)}>
-          <input type="text" name="firstName" key={this.state.firstName}
-            onChange={event => this.handleFirstNameChange(event)}
-            value={this.state.firstName}
-          />
-          <input 
-            type="text" 
-            key={this.state.lastName}
-            onChange={event => this.handleLastNameChange(event)}
-            value={this.state.lastName}
-          />
-          <input type="submit" key="submit-button"/>
-        </form>
-        {this.listOfSubmissions()}
-      </div>
+      <form>
+        <input 
+          type="text" 
+          onChange={event => this.handleFirstNameChange(event)} 
+          name="firstName" value={this.state.firstName} 
+        />
+        <input 
+          type="text" 
+          onChange={event => this.handleLastNameChange(event)} 
+          name="lastName" 
+          value={this.state.lastName} 
+        />
+      </form>
     )
   }
+
 }
 
 export default Form;
